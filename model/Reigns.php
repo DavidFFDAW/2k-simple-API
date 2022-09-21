91820f756ab8cebff3b329d1ee0c124500b9a6f2
davidfernandez@madeinmedia.es <davidfernandez@madeinmedia.es>
2022-09-21 13:55:35 +0200

    feat(reigns): changed sql clause

--- model/Reigns.php
+++ model/Reigns.php
@@ -7,10 +7,10 @@ class Reigns extends ModelModule {
     }
 
     public function getReigns (Request $request) {
-        $sql = "SELECT c.name as championship_name, c.image as ch_img, r.id as reign_id, r.days as reign_days, w.brand as brand, w.name as name, w.image_name as image, w.overall as overall
+        $sql = "SELECT c.name as championship_name, c.image as ch_img, r.id as reign_id, r.days as reign_days, c.brand as brand, w.name as name, w.image_name as image, w.overall as overall
         FROM championship_reigns r INNER JOIN wrestler w 
         ON r.wrestler_id = w.id INNER JOIN championship c ON r.championship_id = c.id
-        WHERE r.current = 1 AND w.status != 'released'";
+        WHERE r.current = 1 AND w.status != 'released' AND c.active = 1";
 
         $row = $this->conn->query($sql);
         $reigns = $row->fetch_all(MYSQLI_ASSOC);
