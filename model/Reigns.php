01bc41c942ba306e7fa9ff55bfa3dc409e9af950
DavidFFDAW <davidferflo2@gmail.com>
2022-10-13 20:15:44 +0200

    feat(Reigns): retrieve tag teams datas with reigns

--- model/Reigns.php
+++ model/Reigns.php
@@ -36,13 +36,13 @@ class Reigns extends DatabaseModel {
         $sql = "SELECT c.name as championship_name, c.image as ch_img, r.id as reign_id, r.days as reign_days, c.brand as brand, w.name as name, w.image_name as image, w.overall as overall
         FROM championship_reigns r INNER JOIN wrestler w 
         ON r.wrestler_id = w.id INNER JOIN championship c ON r.championship_id = c.id
-        WHERE r.current = 1 AND w.status != 'released' AND c.active = 1";
+        WHERE r.current = 1 AND w.status != 'released' AND c.active = 1 AND c.tag = 0";
 
         $row = $this->conn->query($sql);
         $finalChampions = $row->fetch_all(MYSQLI_ASSOC);
 
-        // $tagTeams = $this->getTagTeamsChampions($request);
-        // $finalChampions[] = $tagTeams;
+        $tagTeams = $this->getTagTeamsChampions($request);
+        $finalChampions[] = $tagTeams;
         
         return $this->json->setResponseAndReturn(200, 'Succesful', 'OK', $finalChampions);
     }
